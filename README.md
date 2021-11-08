7f0177dd0bf4a31ef9db7ce1c7f9a32935e57a01
Bryan Fullen <fullenbryan@gmail.com>
2021-11-08 15:24:40 -0800

    testing with proof of work

--- README.md
+++ README.md
@@ -19,3 +19,16 @@ We need a first block on the chain so thats called the genesis block..
 then we need to methods for the block chain like add a new block.
 we will need a method to get the last block in the chain, then we need to check if the chain is not tampered with. it loops over the blocks and sees if the has of each block is correct. it checks if each block points to the correct previous block by compairing the previous hash,. if it works it returns true, if something is off it returns false. 
 
+Adding proof of work.
+Reading about hwo bitcoin creates mining difficulty we will need a Nonce value. 
+so in our block lets add the var nonce to the constructor and initilize its value to 0.
+
+also we will need a method that will increment the nonce value until we get the valid hash. Since its by difficulty we need the difficulty to be a parameter. 
+
+changed the calculateHash() becuase it does not use the nonce var to calculate the hash. 
+
+Now that the block has a nonce and can be mined, we need to ensure that the blockchain will behave correctly by adding a property to track the difficulty. 
+
+I couldnt tell if it was working when i placed it at 2, so i put it at 6 and was still unsure because it was taking too long? (which is a good thing) I set it to 4 and it seems to have the right behavior as i can see that it takes time to mine the block. 
+
+now we need to actually mine the block and then add it to the change...Made some alteration to the method addblock
--- main.js
+++ main.js
@@ -9,16 +9,33 @@ class Block{
     this.previousHash = previousHash;
     //adding property hash, to handle the method of converting to hash256
     this.hash = this.calculateHash();
+    //creating a Nonce Property so we dont incurre a endless loop
+    this.nonce = 0;
   }
   // method to handle hash, takes the properties from class block and hashes them in SHA256 from crypto.js library. 
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + 
+      this.previousHash + 
+      this.timestamp + 
+      JSON.stringify(this.data) + 
+      this.nonce
+      ).toString();
+  }
+
+  // method to make spaming the blockchain harder by adding difficulty the hash to have length of 0's added
+  mineBlock(difficulty){
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("block Mined: " + this.hash);
   }
 }
 // Constructor will initialize the chain but there needs to be a first block on the chain. 
 class Blockchain{
   constructor(){
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
   }
   //Need a method to create the first block the Genisis Block as it is called.
   createGenesisBlock(){
@@ -31,7 +48,8 @@ class Blockchain{
   //Method to add a new block to the chain
   addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    //newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
   //Method to check to see if the block is valid on the blockchain
@@ -55,6 +73,7 @@ class Blockchain{
 let roxyCoin = new Blockchain();
 roxyCoin.addBlock(new Block(1, "01/01/2022", {amount: 4}));
 roxyCoin.addBlock(new Block(2, "01/02/2022", {amount: 10}));
+roxyCoin.addBlock(new Block(3, "01/02/2022", {amount: 110}));
 
 //console.log(JSON.stringify(roxyCoin, null, 4));
 
@@ -66,3 +85,8 @@ roxyCoin.addBlock(new Block(2, "01/02/2022", {amount: 10}));
 
 // Check our chain again (will now return false)
 //console.log("Blockchain valid? " + roxyCoin.isChainValid());
+
+console.log('mining block 1...');
+roxyCoin.addBlock(new Block(1,"01/01/2022", {amount: 4}));
+console.log('mining block 2...');
+roxyCoin.addBlock(new Block(2, "01/02/2022", {amount: 10}));
